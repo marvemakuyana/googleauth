337d56a5c83aa9903c9f624a8d8b280dd03ae6c9
marvemakuyana <marvellousmakuyana@gmail.com>
2023-12-05 14:38:24 +0200

    First commit

---
+++ app/components/UserInfo.jsx
@@ -0,0 +1,5 @@
+import SignInBtn from "./SignInBtn";
+
+export default function UserInfo() {
+  return <SignInBtn />;
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import UserInfo from "./components/UserInfo";
+
+export default function Home() {
+  return (
+    <div className="grid place-items-center h-screen -mt-24">
+      <UserInfo />
+    </div>
+  );
+}
